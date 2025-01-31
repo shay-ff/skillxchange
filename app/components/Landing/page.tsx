ff8c801b0ac5579d49c1417cf03d66b5a8c102e4
dxt <drextor@dxts-Air.cuchdit.in>
2025-01-31 22:55:58 +0530

    second auth

--- app/components/Landing/page.tsx
+++ app/components/Landing/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "../navbar"
+// import { HomePage } from "../HomePage"
 export default function Landing() {
     return (
         <div>
             <Navbar />
+            {/* <HomePage /> */}
         </div>
     )
 };
